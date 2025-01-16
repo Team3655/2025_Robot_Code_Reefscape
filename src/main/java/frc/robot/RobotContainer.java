a1e85fa5ae155f0305b258bb5e454d3e863c3adc
Garrett <garrettp@masonk12.net>
2025-01-16 11:15:59 -0500

    Matt constants changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -178,7 +178,7 @@ public class RobotContainer {
                 drive,
                 () -> -mattTranslation.StickYAxis(),
                 () -> -mattTranslation.StickXAxis(),
-                () -> -mattRotation.StickXAxis()));
+                () -> mattRotation.StickXAxis()));
 
         mattRotation.B1().onTrue(drive.zeroDrive());
 
