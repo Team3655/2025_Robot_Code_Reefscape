d8d7a82885b709fc6ea45e71a53c8d4aa84a52b0
quinnhaygood <62080573+quinnhaygood@users.noreply.github.com>
2025-01-27 15:21:59 -0500

    Joystick Utils improperly applied

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -13,6 +13,10 @@
 
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -21,63 +25,70 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.RobotState;
 import frc.robot.subsystems.drive.DriveConstants;
 import frc.robot.subsystems.drive.DriveSubsystem;
 import frc.robot.util.JoystickUtils;
 
-import java.util.function.DoubleSupplier;
-import frc.robot.RobotState;
-
 public class DriveCommands {
-    private static final double DEADBAND = 0.1;
+  private static final double DEADBAND = 0.1;
 
-    /**
-     * Field relative drive command using two joysticks (controlling linear and
-     * angular velocities).
-     */
-    public static Command joystickDrive(
-            DriveSubsystem drive,
-            DoubleSupplier xSupplier,
-            DoubleSupplier ySupplier,
-            DoubleSupplier omegaSupplier) {
-        return Commands.run(
-                () -> {
-                    // Apply deadband
-                    double linearMagnitude = JoystickUtils.curveInput(xSupplier.getAsDouble(), DEADBAND);
+  private static Translation2d getLinearVelocityFromJoysticks(double x, double y) {
+    // Apply deadband
+    double linearMagnitude = MathUtil.applyDeadband(Math.hypot(x, y), DEADBAND);
+    Rotation2d linearDirection = new Rotation2d(Math.atan2(y, x));
 
-                    Rotation2d linearDirection = 
-                        new Rotation2d(Math.atan2(ySupplier.getAsDouble(), xSupplier.getAsDouble()));
+    Logger.recordOutput("Drive/Commands/joystick magnitude", linearMagnitude);
 
-                    SmartDashboard.putNumber("LinearMagnitude", linearMagnitude);
-                    SmartDashboard.putNumber("linearDirection", linearDirection.getDegrees());
+    // Square magnitude for more precise control
+    linearMagnitude = JoystickUtils.curveInput(linearMagnitude, DEADBAND);
 
-                    double omega = MathUtil.applyDeadband(omegaSupplier.getAsDouble(), DEADBAND);    
+    // Return new linear velocity
+    return new Pose2d(new Translation2d(), linearDirection)
+        .transformBy(new Transform2d(linearMagnitude, 0.0, new Rotation2d()))
+        .getTranslation();
+  }
 
-                    // Square values
-                    linearMagnitude = linearMagnitude * linearMagnitude;
-                    omega = Math.copySign(omega * omega, omega);
+  /**
+   * Field relative drive command using two joysticks (controlling linear and
+   * angular velocities).
+   */
+  public static Command joystickDrive(
+      DriveSubsystem drive,
+      DoubleSupplier xSupplier,
+      DoubleSupplier ySupplier,
+      DoubleSupplier omegaSupplier) {
+    return Commands.run(
+        () -> {
 
-                    // Calculate new linear velocity
-                    Translation2d linearVelocity = new Pose2d(new Translation2d(), linearDirection)
-                            .transformBy(new Transform2d(linearMagnitude, 0.0, new Rotation2d()))
-                            .getTranslation();
+          Translation2d linearVelocity = getLinearVelocityFromJoysticks(xSupplier.getAsDouble(),
+              ySupplier.getAsDouble());
 
-                    // Convert to field relative speeds & send command
-                    boolean isFlipped = DriverStation.getAlliance().isPresent()
-                            && DriverStation.getAlliance().get() == Alliance.Red;
-                    drive.runVelocity(
-                            ChassisSpeeds.fromFieldRelativeSpeeds(
-                                    linearVelocity.getX() * DriveConstants.MAX_LINEAR_SPEED,
-                                    linearVelocity.getY() * DriveConstants.MAX_LINEAR_SPEED,
-                                    omega * DriveConstants.MAX_ANGULAR_SPEED,
-                                    isFlipped
-                                            ? RobotState.getInstance().getEstimatedPose().getRotation()
-                                                    .plus(new Rotation2d(Math.PI))
-                                            : RobotState.getInstance().getEstimatedPose().getRotation()));
-                },
-                drive);
-    }
+          double omega = JoystickUtils.curveInput(omegaSupplier.getAsDouble(), DEADBAND);
+
+          Logger.recordOutput("Drive/Commands/omega", omega);
+          Logger.recordOutput("Drive/Commands/linear velocity", linearVelocity);
+
+          // Convert to field relative speeds & send command
+          ChassisSpeeds speeds =
+              new ChassisSpeeds(
+                  linearVelocity.getX() * DriveConstants.MAX_LINEAR_SPEED,
+                  linearVelocity.getY() * DriveConstants.MAX_LINEAR_SPEED,
+                  omega * DriveConstants.MAX_ANGULAR_SPEED);
+
+          boolean isFlipped =
+              DriverStation.getAlliance().isPresent()
+                  && DriverStation.getAlliance().get() == Alliance.Red;
+                  
+          drive.runVelocity(
+              ChassisSpeeds.fromFieldRelativeSpeeds(
+                  speeds,
+                  isFlipped
+                      ? RobotState.getInstance().getRotation().plus(new Rotation2d(Math.PI))
+                      : RobotState.getInstance().getRotation()));
+        },
+        drive);
+  }
 }
