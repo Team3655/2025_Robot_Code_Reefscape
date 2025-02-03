420da099346e6949e8a61eed13d957ff7e70b879
Henry Mullin <hdmullin1057@gmail.com>
2025-02-03 14:30:08 -0500

    intake sim because why not

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -5,10 +5,16 @@ import com.ctre.phoenix6.CANBus;
 import edu.wpi.first.math.util.Units;
 
 public class IntakeConstants {
+
   public static final CANBus BUS = new CANBus("rio");
   public static final int VAC_MOTOR_PORT = 40;
   public static final int INTAKE_MOTOR_PORT = 41;
   public static final int CANRANGE_PORT = 42;
+
+  public static final double INTAKE_JKG = 0.025;
+  public static final double INTAKE_GEARING = 1;
+  public static final double VACUUM_JKG = 0.025;
+  public static final double VACUUM_GEARING = 1;
 
   public static final double CANRANGE_DETECTION_RANGE = Units.inchesToMeters(3);
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeIOSIM.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSIM.java
@@ -1,11 +1,17 @@
 package frc.robot.subsystems.intake;
 
-public class IntakeIOSIM implements IntakeIO {
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+public class IntakeIOSim implements IntakeIO {
 
   // I am completely useful :D
-
   /*
-   *                   ,--.    ,--.
+   *              ,--.    ,--.
                  ((O ))--((O ))
                ,'_`--'____`--'_`.
               _:  ____________  :_
@@ -20,7 +26,7 @@ public class IntakeIOSIM implements IntakeIO {
       /`.\:| | ||  __  __  __  || | |;/,'\
      :`-._\;.| || '--''--''--' || |,:/_.-':
      |    :  | || .----------. || |  :    |
-     |    |  | || '----SSt---' || |  |    |
+     |    |  | || '----------' || |  |    |
      |    |  | ||   _   _   _  || |  |    |
      :,--.;  | ||  (_) (_) (_) || |  :,--.;
      (`-'|)  | ||______________|| |  (|`-')
@@ -34,6 +40,39 @@ public class IntakeIOSIM implements IntakeIO {
               |        ||        |
               '--------''--------'
    */
+  // This is wonderful, thank you Mr. Garrett.
 
-     
+  // Real intake uses kraken 44, but it doesnt matter too much for sim.
+  private static final DCMotor INTAKE_GEARBOX = DCMotor.getKrakenX60( 1);
+  private static final DCMotor VACUUM_GEARBOX = DCMotor.getKrakenX60(1);
+  private DCMotorSim intakeMotor;
+  private DCMotorSim vacuumMotor;
+
+  public IntakeIOSim(){
+    intakeMotor =  new DCMotorSim(LinearSystemId.createDCMotorSystem(INTAKE_GEARBOX, IntakeConstants.INTAKE_JKG, IntakeConstants.INTAKE_GEARING), INTAKE_GEARBOX);
+    vacuumMotor = new DCMotorSim(LinearSystemId.createDCMotorSystem(VACUUM_GEARBOX, IntakeConstants.VACUUM_JKG, IntakeConstants.VACUUM_GEARING), VACUUM_GEARBOX);
+  }
+
+  @Override
+  public void updateInputs(IntakeIOInputs inputs){
+    inputs.intakeAppliedVolts = intakeMotor.getInputVoltage();
+    inputs.intakeCurrentAmps = new double[] { intakeMotor.getCurrentDrawAmps() };
+    inputs.intakeVelocityRadPerSec = intakeMotor.getAngularVelocityRadPerSec();
+    
+    inputs.vacuumAppliedVolts = vacuumMotor.getInputVoltage();
+    
+    Logger.recordOutput("Intake/VelocityRadPerSec", intakeMotor.getAngularVelocityRadPerSec());
+    Logger.recordOutput("Intake/AppliedVolts", intakeMotor.getInputVoltage());
+    Logger.recordOutput("Intake/vacuumAppliedVolts", vacuumMotor.getInputVoltage());
+  }
+
+  @Override 
+  public void setVoltage(double volts){
+    intakeMotor.setInputVoltage(MathUtil.clamp(volts, -12, 12));
+  }
+
+  @Override
+  public void setVacuumVoltage(double volts){
+    vacuumMotor.setInputVoltage(MathUtil.clamp(volts, -12, 12));
+  }
 }
